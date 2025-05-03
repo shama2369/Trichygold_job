e4e792e74e44afa455659d21c2e56195c56819f6
shama2369@gmail.com <shama2369@gmail.com>
2025-05-04 02:42:33 +0400

    Fix build:css with input.css and tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          600: "#9F7AEA",
+          700: "#6B46C1",
+          800: "#553C9A",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
